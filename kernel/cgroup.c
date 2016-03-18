13cc1aae03933b12ac3c07ea022dec9db37a902a
Serge Hallyn <serge.hallyn@ubuntu.com>
2016-03-18 12:34:34 -0700

    split out nsroot printing to its own fn
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -1565,14 +1565,37 @@ static int rebind_subsystems(struct cgroup_root *dst_root,
 	return 0;
 }
 
+static void cgroup_show_nsroot(struct seq_file *seq, struct dentry *dentry,
+			       struct kernfs_root *kf_root)
+{
+	struct kernfs_node *d_kn = dentry->d_fsdata;
+	char *nsroot;
+	int len, ret;
+
+	if (!kf_root)
+		return;
+	len = kernfs_path_from_node(d_kn, kf_root->kn, NULL, 0);
+	if (len <= 0)
+		return;
+	nsroot = kzalloc(len + 1, GFP_ATOMIC);
+	if (!nsroot)
+		return;
+	ret = kernfs_path_from_node(d_kn, kf_root->kn, nsroot, len + 1);
+	if (ret <= 0 || ret > len)
+		goto out;
+
+	seq_show_option(seq, "nsroot", nsroot);
+
+out:
+	kfree(nsroot);
+}
+
 static int cgroup_show_options(struct seq_file *seq, struct dentry *dentry,
 			       struct kernfs_root *kf_root)
 {
 	struct cgroup_root *root = cgroup_root_from_kf(kf_root);
 	struct cgroup_subsys *ss;
 	int ssid;
-	char nsroot[256];
-	struct kernfs_node *d_kn = dentry->d_fsdata;
 
 	if (root != &cgrp_dfl_root)
 		for_each_subsys(ss, ssid)
@@ -1593,10 +1616,8 @@ static int cgroup_show_options(struct seq_file *seq, struct dentry *dentry,
 		seq_puts(seq, ",clone_children");
 	if (strlen(root->name))
 		seq_show_option(seq, "name", root->name);
-	if (kernfs_path(d_kn, nsroot, 256))
-		seq_show_option(seq, "nsroot", nsroot);
-	else
-		seq_show_option(seq, "nsroot", "invalid");
+	cgroup_show_nsroot(seq, dentry, kf_root);
+
 	return 0;
 }
 
